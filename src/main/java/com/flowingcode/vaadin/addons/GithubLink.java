941f86183210bc50d501efb912b425f36a9a8aa3
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2020-11-10 14:55:58 -0300

    Add GithubLink annotation

---
+++ src/main/java/com/flowingcode/vaadin/addons/GithubLink.java
@@ -0,0 +1,19 @@
+package com.flowingcode.vaadin.addons;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used for configuring the URL in the {@code GitHubCorner}
+ * @author Javier Godoy / Flowing Code
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface GithubLink {
+	String value();
+}
