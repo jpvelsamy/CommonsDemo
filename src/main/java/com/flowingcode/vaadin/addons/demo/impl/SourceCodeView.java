18c601e427639d97993c4a6457c69f2e6d211a0d
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2020-11-16 10:16:40 -0300

    Make SourceCodeView and SplitLayoutDemo package-private

--- src/main/java/com/flowingcode/vaadin/addons/demo/impl/SourceCodeView.java
+++ src/main/java/com/flowingcode/vaadin/addons/demo/impl/SourceCodeView.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.html.IFrame;
 
 @SuppressWarnings("serial")
-public class SourceCodeView extends Composite<IFrame> {
+class SourceCodeView extends Composite<IFrame> {
 
 	public SourceCodeView(String sourceUrl) {
 		getContent().getElement().setAttribute("frameborder", "0");
